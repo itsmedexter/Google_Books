1bbad5764f7696ee8601416b3a8768aa5f87f7a7
Dexter Valencia <deeside@pacbell.net>
2020-02-18 22:31:12 -0800

    updated server.js, installed axios and mongoose

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactgooglebooks");
+
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +16,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.us(routes);
 
 // Send every other request to the React app
 // Define any API routes before this runs
