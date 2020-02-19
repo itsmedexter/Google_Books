22569f250404d7e2ec70fa5c3378fcf694b1d391
Dexter Valencia <deeside@pacbell.net>
2020-02-19 00:31:26 -0800

    added components

---
+++ client/src/components/EmptyList/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EmptyList(){
+    return(
+        <div>
+            <h3>No Books</h3>
+        </div>
+    )
+}
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Nav() {
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        React Google Book
+      </a>
+      <ul className="navbar-nav">
+     <li className="nav-item">
+       <a className="nav-link navbar-brand" id="savedLink" href="/bookshelf">Saved Books</a>
+     </li>
+   </ul>
+    </nav>
+  );
+}
+
+export default Nav;
